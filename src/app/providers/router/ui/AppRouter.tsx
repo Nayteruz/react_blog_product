8ff337e9a5e937147e0754fb17c9a123305b6b06
Nayteruz <nayteruz@gmail.com>
2023-02-15 00:25:44 +0700

    Убрал лишний suspense

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -5,22 +5,20 @@ import {Layout} from "shared/ui/Layout";
 
 export const AppRouter = () => {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                {Object.values(routeConfig).map(({path, element}) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={(
-                            <Suspense fallback={<div>Loading...</div>} >
-                                <Layout className="content-page--text-content">
-                                    {element}
-                                </Layout>
-                            </Suspense>
-                        )}
-                    />
-                ))}
-            </Routes>
-        </Suspense>
+        <Routes>
+            {Object.values(routeConfig).map(({path, element}) => (
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <Layout className="content-page--text-content">
+                                {element}
+                            </Layout>
+                        </Suspense>
+                    )}
+                />
+            ))}
+        </Routes>
     );
 };
