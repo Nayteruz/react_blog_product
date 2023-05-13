12f32d2805ab058b3056e882722de713eb8204df
Nayteruz <nayteruz@gmail.com>
2023-05-13 22:19:51 +0700

    Добавил все изменения

--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.tsx
@@ -1,8 +1,4 @@
 import { FC, lazy } from 'react';
 import { AddCommentFormProps } from './AddCommentForm';
 
-export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    // Так в реальных проектах делать не стоит!!!
-    setTimeout(() => resolve(import('./AddCommentForm')), 1500);
-}));
+export const AddCommentFormAsync = lazy<FC<AddCommentFormProps>>(() => import('./AddCommentForm'));
