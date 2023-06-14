623fe8fbcac4db089ea4b3761533b86097117563
Nayteruz <nayteruz@gmail.com>
2023-06-15 00:42:03 +0700

    fix storybook, add mocks for notification list, article rating

--- src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
+++ src/features/articleRating/ui/ArticleRating/ArticleRating.stories.tsx
@@ -1,16 +1,59 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-
+import withMock from 'storybook-addon-mock';
 import ArticleRating from './ArticleRating';
+import { StoryDecorator } from '@/shared/config/storybook/StoryDecorator/StoryDecorator';
 
 export default {
-    title: 'shared/ArticleRating',
+    title: 'features/ArticleRating',
     component: ArticleRating,
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [withMock],
 } as ComponentMeta<typeof ArticleRating>;
 
 const Template: ComponentStory<typeof ArticleRating> = (args) => <ArticleRating {...args} />;
 
 export const Normal = Template.bind({});
-Normal.args = {};
+Normal.args = {
+    articleId: '1',
+};
+Normal.decorators = [StoryDecorator({
+    user: {
+        authData: { id: '1' },
+    },
+})];
+Normal.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/article-ratings?userId=1&articleId=1`,
+            method: 'GET',
+            status: 200,
+            response: [
+                {
+                    rate: 4,
+                },
+            ],
+        },
+    ],
+};
+
+export const WithoutRate = Template.bind({});
+WithoutRate.args = {
+    articleId: '1',
+};
+WithoutRate.decorators = [StoryDecorator({
+    user: {
+        authData: { id: '1' },
+    },
+})];
+WithoutRate.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/article-ratings?userId=1&articleId=1`,
+            method: 'GET',
+            status: 200,
+            response: [],
+        },
+    ],
+};
