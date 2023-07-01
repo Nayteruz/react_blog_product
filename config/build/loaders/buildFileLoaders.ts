9dd624545f5dae0381c736e72a93b9c369a368ab
Nayteruz <nayteruz@gmail.com>
2023-07-01 21:17:56 +0700

    Обновил конфигурацию webpack, добавил babel cache

--- config/build/loaders/buildFileLoaders.ts
+++ config/build/loaders/buildFileLoaders.ts
@@ -1,6 +1,7 @@
 export function buildFileLoaders() {
     return {
         test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+        exclude: /node_modules/,
         use: [
             {
                 loader: 'file-loader',
